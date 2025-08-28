ce2a1de6a6e77fd26b407ff511e35c1b2a7c8065
Muhammad Ridhwan Khalid <m.ridhwankhalid@gmail.com>
2025-08-28 17:59:27 +1000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const poppins = Poppins({
 })
 
 export const metadata: Metadata = {
-  title: "Store'em",
+  title: "StoreIt",
   description: "Store'em - The only storage solution you need.",
 };
 
